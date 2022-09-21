ca5fe0b19187d3a9874994e05b037cdbb890d73c
Pablo Ramos Muras <pabloramosmuras@gmail.com>
2022-09-21 19:22:05 +0200

    fix: Refactored function updateQuality

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -1,5 +1,10 @@
+const MIN_QUALITY = 0;
+const MAX_QUALITY = 50;
+const MIN_SELLIN = 0;
+const SELLIN_BACKSTAGE_10 = 11;
+const SELLIN_BACKSTAGE_5 = 6;
 class Item {
-  constructor(name, sellIn, quality){
+  constructor(name, sellIn, quality) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
@@ -7,61 +12,76 @@ class Item {
 }
 
 class Shop {
-  constructor(items=[]){
+  constructor(items = []) {
     this.items = items;
   }
+
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
+      if (this.items[i].name === "Sulfuras, Hand of Ragnaros") {
+        continue;
+      }
+      if (this.items[i].name === "Backstage passes to a TAFKAL80ETC concert") {
+        this.updateBackstagePasses(this.items[i]);
+        continue;
+      }
+      if (this.items[i].name === "Aged Brie") {
+        this.updateAgedBrie(this.items[i]);
+        continue;
+      }
+
+      this.updateNormalItem(this.items[i]);
+      return this.items;
+
+    }
+  }
+
+  updateNormalItem(item) {
+    if (item.quality > MIN_QUALITY) {
+      item.quality = item.quality - 1;
+    }
+    item.sellIn = item.sellIn - 1;
+    if (item.sellIn < MIN_SELLIN) {
+      if (item.quality > MIN_QUALITY) {
+        item.quality = item.quality - 1;
+      }
+    }
+  }
+
+  updateAgedBrie(item) {
+    if (item.quality < MAX_QUALITY) {
+      item.quality = item.quality + 1;
+    }
+    item.sellIn = item.sellIn - 1;
+    if (item.sellIn < MIN_SELLIN) {
+      if (item.quality < MAX_QUALITY) {
+        item.quality = item.quality + 1;
+      }
+    }
+  }
+
+  updateBackstagePasses(item) {
+    if (item.quality < MAX_QUALITY) {
+      item.quality = item.quality + 1;
+      if (item.sellIn < SELLIN_BACKSTAGE_10) {
+        if (item.quality < MAX_QUALITY) {
+          item.quality = item.quality + 1;
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
+      if (item.sellIn < SELLIN_BACKSTAGE_5) {
+        if (item.quality < MAX_QUALITY) {
+          item.quality = item.quality + 1;
         }
       }
     }
-
-    return this.items;
+    item.sellIn = item.sellIn - 1;
+    if (item.sellIn < MIN_SELLIN) {
+      item.quality = 0;
+    }
   }
 }
 
 module.exports = {
   Item,
-  Shop
-}
+  Shop,
+};
