99b35a1a7b21d140de601d603b516737710a12ea
pbermejo <pbermejo.dev@gmail.com>
2022-09-22 19:39:01 +0200

    Partial refactor improving readability

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -1,63 +1,132 @@
+/**
+ * Item class is dumb and used as dataclass
+ */
 class Item {
   constructor(name, sellIn, quality){
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
   }
+
+  setQuality(newQuality){
+    this.quality = newQuality
+  }
+
+  setSellIn(newSellIn){
+    this.SellIn = newSellIn
+  }
+
+  upgradeQuality(delta){
+    let upgradedQuality = this.quality + delta
+    this.setQuality(newQuality)
+  }
+  
+  downgradeQuality(delta){
+    let downgradedQuality = this.quality - delta
+    this.setQuality(downgradedQuality)
+  }
+  
+  downgradeSellIn(delta){
+    let downgradedSellIn = this.sellIn - delta
+    this.setQuality(downgradedSellIn)
+  }
 }
 
+
+/**
+ * Shop class knows about quality and sell in logic
+ */
 class Shop {
+
+  QUALITY_MIN_BOUND = 0
+  QUALITY_MAX_BOUND = 50
+  QUALITY_DELTA = 1
+  QUALITY_DELTA_DOUBLE = 2
+  QUALITY_DELTA_TRIPLE = 3
+  SELL_IN_DELTA = 1
+  SELL_IN_MIN_BOUND = 0
+  SELL_IN_MIDDLE_BOUND = 6
+  SELL_IN_MAX_BOUND = 11
+  AGED_BRIE = 'Aged Brie'
+  BACKSTAGE = 'Backstage passes to a TAFKAL80ETC concert'
+  SULFURAS = 'Sulfuras, Hand of Ragnaros'
+
   constructor(items=[]){
     this.items = items;
   }
+
   updateQuality() {
+
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
+      let currentItem = this.items[i]
+      updateQualityOnItem(currentItem)   
     }
 
     return this.items;
+  }
+
+  updateQualityOnItem(currentItem){
+
+      const isAgedBrie = (currentItem.name == this.AGED_BRIE)
+      const isBackstage = (currentItem.name == this.BACKSTAGE)
+      const isSulfuras = (currentItem.name == this.SULFURAS)
+      
+      const hasBadQuality = (currentItem.quality < this.QUALITY_MAX_BOUND)
+      const hasGoodQuality = (currentItem.quality  > this.QUALITY_MIN_BOUND)
+      const isBelowMaxSellIn = (currentItem.sellIn < this.SELL_IN_MAX_BOUND)
+      const isBelowHalfSellIn = (currentItem.sellIn < this.SELL_IN_MIDDLE_BOUND)
+      const isBelowMinSellIn = (currentItem.sellIn < this.SELL_IN_MIN_BOUND)
+
+
+      /**
+       * Wicked logic. Don't know how to handle it
+       */
+
+      if (!isAgedBrie && !isBackstage) {
+        if(hasGoodQuality){
+          if (!isSulfuras) {
+            currentItem.downgradeQuality(this.QUALITY_DELTA);
+          }
+        }
+      } else {
+        if (hasBadQuality) {
+          currentItem.upgradeQuality(this.QUALITY_DELTA)
+          if (isBackstage) {
+            if (isBelowMaxSellIn) {
+              if (hasBadQuality) {
+                currentItem.upgradeQuality(this.QUALITY_DELTA)
+              }
+            }
+            if (isBelowHalfSellIn) {
+              if (hasBadQuality) {
+                currentItem.upgradeQuality(this.QUALITY_DELTA)
+              }
+            }
+          }
+        }
+      }
+
+      if (!isSulfuras) {
+        currentItem.downgradeSellIn(this.SELL_IN_DELTA)
+      }
+
+      if (isBelowMinSellIn) {
+        if (!isAgedBrie) {
+          if (!isBackstage) {
+            if (hasGoodQuality) {
+              if (!isSulfuras) {
+                currentItem.downgradeQuality(this.QUALITY_DELTA);
+              }
+            }
+          } else {
+            currentItem.downgradeQuality(currentItem.quality);
+          }
+        } else {
+          if (hasBadQuality) {
+            currentItem.upgradeQuality(this.QUALITY_DELTA)
+          }
+        }
+      }
   }
 }
 
