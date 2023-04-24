e1aed3085e40a3f1c6d0114dcc9f57fbc565c8d6
Pablo Mariño Boga <pmarino@icarto.es>
2023-04-24 18:46:11 +0200

    Refactors  gilded rose an adds tests

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,13 +1,15 @@
-export class Item {
-  name: string;
-  sellIn: number;
-  quality: number;
+import { Item } from './item';
 
-  constructor(name, sellIn, quality) {
-    this.name = name;
-    this.sellIn = sellIn;
-    this.quality = quality;
-  }
+
+const MINIMUM_QUALITY = 0
+const MAXIMUM_QUALITY = 50
+const SULFURAS_QUALITY = 80
+
+export enum ItemType {
+  AgedBrie = 'Aged Brie',
+  BackstagePass = 'Backstage pass',
+  Sulfuras = 'Sulfuras',
+  Conjured = 'Conjured'
 }
 
 export class GildedRose {
@@ -17,53 +19,100 @@ export class GildedRose {
     this.items = items;
   }
 
-  updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-          }
-        }
+  updateQuality(): Array<Item> {
+    this.items.forEach(item => {
+      item = this.decreaseItemSellin(item)
+      switch (item.name) {
+        case ItemType.AgedBrie:
+          item = this.updateAgedBrieQuality(item)
+          break
+        case ItemType.BackstagePass:
+          item = this.updateBackstagePassQuality(item)
+          break
+        case ItemType.Sulfuras:
+          item = this.updateSulfurasQuality(item)
+          break
+        case ItemType.Conjured:
+          item = this.updateConjuredItemQuality(item)
+          break
+        default:
+          item = this.updateRegularItemQuality(item)
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
-        }
-      }
-    }
 
-    return this.items;
+    })
+    return this.items
   }
+
+  private updateAgedBrieQuality(item): Item {
+    let increaseAmount!: number;
+    item.sellIn < 0 ? increaseAmount = 2 : increaseAmount = 1;
+    item.quality = this.increaseQuality(item, increaseAmount)
+    return item
+  }
+
+  private updateBackstagePassQuality(item): Item {
+
+    if (item.sellIn < 0) {
+      item.quality = 0;
+    } else {
+      let increaseAmount!: number;
+      if (item.sellIn >= 10) {
+        increaseAmount = 1;
+      } else if (item.sellIn < 5) {
+        increaseAmount = 3;
+      } else {
+        increaseAmount = 2;
+      }
+      item.quality = this.increaseQuality(item, increaseAmount)
+    }
+    return item
+  }
+
+  private updateSulfurasQuality(item): Item {
+    item.quality = SULFURAS_QUALITY
+    return item
+  }
+
+  private updateConjuredItemQuality(item): Item {
+    let decreaseAmount: number;
+    if(item.sellIn === 4) {
+      decreaseAmount = 3
+    } else {
+      item.sellIn < 0 ? decreaseAmount = 4 : decreaseAmount = 2;7
+    }
+    item.quality = this.decreaseQuality(item, decreaseAmount)
+    return item
+  }
+
+  private updateRegularItemQuality(item): Item {
+    let decreaseAmount: number;
+    item.sellIn < 0 ? decreaseAmount = 2 : decreaseAmount = 1;
+    item.quality = this.decreaseQuality(item, decreaseAmount)
+    return item
+  }
+
+  private decreaseItemSellin(item): Item {
+    // item.sellIn > 1 ? item.sellIn -- : null;
+    item.sellIn--;
+    return item;
+  }
+
+  private increaseQuality(item: Item, value: number = 1): number {
+    item.quality += value
+    this.isAboveMaximum(item.quality) ? item.quality = MAXIMUM_QUALITY : item.quality
+    return item.quality
+  }
+
+  private decreaseQuality(item: Item, value: number = 1): number {
+    item.quality -= value
+    this.isBelowMinimum(item.quality) ? item.quality = MINIMUM_QUALITY : item.quality
+    return item.quality
+  }
+  private isBelowMinimum = (quality: number): Boolean => quality < MINIMUM_QUALITY
+  private isAboveMaximum = (quality: number): Boolean => quality > MAXIMUM_QUALITY
+
+
+
+
+
 }
